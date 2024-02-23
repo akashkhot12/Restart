5767f2ce7affe1763d3c54dd73cafcef92906788
akashkhot12 <akashkhot03@gmail.com>
2024-02-23 09:28:12 +0530

    add new crud

--- No&Ex/crud/connections.js
+++ No&Ex/crud/connections.js
@@ -1,14 +1,12 @@
-const {MongoClient, Collection}= require("mongodb");
+const {MongoClient}= require("mongodb");
 const url  = "mongodb://localhost:27017";
 const client = new MongoClient(url);
 const database  = 'Restart';
 
 
-const getConnections = async()=>{
+const getConnect = async()=>{
     let result = await client.connect();
-    let db = await result.db(database);
-    return db.collection('restart');
+    let db  = await result.db(database);
+    return db.collection('restart')
 }
-
-
-module.export = getConnections;
+module.exports = getConnect;
--- No&Ex/crud/index.js
+++ No&Ex/crud/index.js
@@ -1,3 +1,31 @@
 const express = require('express');
-const mongodb = require('./connections')
+const db = require('./connections')
 const app = express();
+
+app.use(express.json());
+
+app.get('/',async(req,res)=>{
+    let data =  await db();
+    let getData = await data.find().toArray();
+    res.status(201).json({message:getData});
+    console.log(getData);
+});
+
+app.post('/',async(req,res)=>{
+    let data = await db();
+    let addData = await data.insertOne({
+        firstName:req.body,
+        LastName:req.body,
+        gender:req.body
+    })
+    res.status(201).json({message:"inserted Successfully",addData})
+})
+
+app.put('/:id',async(req,res)=>{
+    
+})
+
+
+app.listen(5000,()=>{
+    console.log('server is up on 5000');
+})
