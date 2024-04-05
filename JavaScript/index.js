0548558b50a569bd4d6c920a5aa7ec45a38e4294
akashkhot12 <akashkhot03@gmail.com>
2024-04-05 12:10:46 +0530

    adding index

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,14 +1,14 @@
 let marks = prompt("please neter your marks ")
 
-if (marks>=90 || marks<=100) {
+if (marks>=90 && marks<=100) {
     console.log("grade A");
-}else if (marks>=70 || marks<=89) {
+}else if (marks>=70 &&marks<=89) {
     console.log("grade B");
 }
-else if (marks>=60 || marks<=69) {
+else if (marks>=60 && marks<=69) {
     console.log("grade C");
 }
-else if (marks>=50 || marks<=59) {
+else if (marks>=50 && marks<=59) {
     console.log("grade D");
 }else {
     console.log("invalid digits");
